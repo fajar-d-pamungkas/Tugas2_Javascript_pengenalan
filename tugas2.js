423c5565175d8d844439f8adbd41e164bf10d183
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-09-18 10:40:48 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var angka = 14
+var huruf = "Huruf"
+var kondisi= true
+var kosong = null
+var data = ["Fajar","Destiawan","Pamungkas"]
+
+console.log(angka)
+console.log(huruf)
+console.log(kondisi)
+console.log(null)
+console.log(data)
